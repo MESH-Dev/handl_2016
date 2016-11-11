8b9b8a35b8fbed90638f1f8c7f751333737a4ff3
shaundover <shaun@meshfresh.com>
2016-11-11 00:48:44 +0000

    Correct &larr; issue on functions, layout on index.php

--- index.php
+++ index.php
@@ -22,70 +22,81 @@ get_header(); ?>
     </div>
     
     <div class="nine columns">
-    <div id='interior-header' class='transparent-blue'>
-        
-        <h1>News</h1>
+	    <div id='interior-header' class='transparent-blue'>
+	        
+	        <h1>News</h1>
 
-    </div>
+	    </div><!--end interior-header-->
     
-    <div id='interior-content' class='transparent-gray'>
-		<div class="entry-content entry-list">
-			<?php if ( have_posts() ) : ?>
+	    <div id='interior-content' class='transparent-gray'>
+			<div class="entry-content entry-list">
+				<?php if ( have_posts() ) : ?>
 
-				<?php twentyeleven_content_nav( 'nav-above' ); ?>
+					<?php twentyeleven_content_nav( 'nav-above' ); ?>
 
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-                
-                	<h2><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-                    
-                    <?php twentyeleven_posted_on(); ?>
-                    
-                    <? 
-						//if ( has_post_thumbnail() ):
-						//	the_post_thumbnail( array(210,220) ); 
-						//else: ?>
-                         
-					<?  //endif; ?>
-                    
-                    <div id='post-content'>
+					<?php /* Start the Loop */ ?>
+					<?php while ( have_posts() ) : the_post(); ?>
+	                
+	                	<h2>
+	                		<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+	                	</h2>
+	                    
+	                    <?php twentyeleven_posted_on(); ?>
+	                    
+	                    <? 
+							//if ( has_post_thumbnail() ):
+							//	the_post_thumbnail( array(210,220) ); 
+							//else: ?>
+	                         
+						<?  //endif; ?>
+	                    
+	                    <div id='post-content'>
 
-						<?php get_template_part( 'content', 'page' ); ?>
-                
-                        <?php comments_template( '', true ); ?>
-                        
-                        <div class='blog-social-media'>
-                        	<span class='st_email_custom' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>'></span>
-                            <span class="st_linkedin_custom" displaytext="LinkedIn" st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>'></span>						<span class='st_sharethis_custom' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>'></span>
-                            <span class='st_twitter_custom' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>'></span>
-                            <span class='st_facebook_custom' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>'></span>
-                       </div>
-                        
-                        <a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark" class='read-more'>Read More...</a>
-                        
-                    </div>
+							<?php get_template_part( 'content', 'page' ); ?>
+	                
+	                        <?php comments_template( '', true ); ?>
+	                        
+	                        <div class="row">
+	                        	<div class="twelve columns">
+			                        <div class='blog-social-media'>
+			                        	<span class='st_email_custom' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>'></span>
+			                            <span class="st_linkedin_custom" displaytext="LinkedIn" st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>'></span>						
+			                            <span class='st_sharethis_custom' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>'></span>
+			                            <span class='st_twitter_custom' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>'></span>
+			                            <span class='st_facebook_custom' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>'></span>
+			                       </div>
+	                        
+	                        		<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark" class='read-more'>Read More...</a>
+	                        	</div>
+                        	</div>
+	                    </div><!--end #post-content-->
 
-					<?php //get_template_part( 'content', get_post_format() ); ?>
+						<?php //get_template_part( 'content', get_post_format() ); ?>
 
-				<?php endwhile; ?>
+					<?php endwhile; ?>
 
-				<?php twentyeleven_content_nav( 'nav-below' ); ?>
+					<div class="row">
+					<div class="twelve columns">
+							<?php twentyeleven_content_nav( 'nav-below' ); ?>
+						</div>
+					</div>
 
-			<?php else : ?>
+				<?php else : ?>
 
-				<article id="post-0" class="post no-results not-found">
-					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
-					</header><!-- .entry-header -->
+					<article id="post-0" class="post no-results not-found">
+						<header class="entry-header">
+							<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
+						</header><!-- .entry-header -->
 
-					<div class="entry-content">
-						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?></p>
-						<?php get_search_form(); ?>
-					</div><!-- .entry-content -->
-				</article><!-- #post-0 -->
+						<div class="entry-content">
+							<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?></p>
+							<?php get_search_form(); ?>
+						</div><!-- .entry-content -->
+					</article><!-- #post-0 -->
 
-			<?php endif; ?>
-		</div>
-	</div>
-   </div>     
+				<?php endif; ?>
+			</div> <!-- end entry-content -->
+		</div><!-- end #interior-content -->
+	<!-- </div> -->
+   </div> <!--end nine columns -->    
 <?php get_footer(); ?>
